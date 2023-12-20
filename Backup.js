270fe41c86d85563cc43868b9ace97e842fc4fc3
Mayson Lee <maysonlee@Maysons-MacBook-Air.local>
2023-12-20 09:39:59 -0800

    Cards base

--- Backup.js
+++ Backup.js
@@ -80,3 +80,79 @@
 //   };
 
 //   export default App;
+
+////////////////////////////////////////////////////////
+
+// import React, { Component, useEffect, useState } from 'react';
+// import '../scss/style.scss'
+// import Kobe from '../assets/KobeBryant.jpg'
+// import Bill from '../assets/BillRussell.jpeg'
+// import Kareem from '../assets/Kareem.jpeg'
+// import Larry from '../assets/LarryBird.jpeg'
+// import Lebron from '../assets/LebronJames.jpeg'
+// import Magic from '../assets/MagicJohnson.jpeg'
+// import Michael from '../assets/MichaelJordan.jpeg'
+// import Shaq from '../assets/Shaq.jpeg'
+// import Wilt from '../assets/WiltChamberlain.jpeg'
+
+// const cardData = [
+//   { player: Kobe, id: 1 },
+//   { player: Michael, id: 2 },
+//   { player: Lebron, id: 3 },
+//   { player: Kareem, id: 4 },
+// ];
+
+// const App = () => {
+//   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+//   useEffect(() => {
+//     const handleOnMouseMove = (e) => {
+//       const { currentTarget: target } = e;
+//       const rect = target.getBoundingClientRect();
+//       const x = e.clientX - rect.left;
+//       const y = e.clientY - rect.top;
+
+//       setMousePosition({ x, y });
+//     };
+
+//     const cards = document.querySelectorAll('.card');
+
+//     cards.forEach((card) => {
+//       card.addEventListener('mousemove', handleOnMouseMove);
+
+//       return () => {
+//         card.removeEventListener('mousemove', handleOnMouseMove);
+//       };
+//     });
+
+//     return () => {
+//       cards.forEach((card) => {
+//         card.removeEventListener('mousemove', handleOnMouseMove);
+//       });
+//     };
+//   }, []); 
+
+//   return (
+//     <>
+//       <h1 id="header">Greatest Of All Time</h1>
+//       <div id="cards">
+//         {cardData.map((card) => (
+//           <div
+//             key={card.id}
+//             className="card"
+//             style={{
+//               '--mouse-x': `${mousePosition.x}px`,
+//               '--mouse-y': `${mousePosition.y}px`,
+//               backgroundImage: `url(${card.player})`,
+//               backgroundSize: 'cover',
+//               backgroundPosition: 'center',
+//             }}
+//           ></div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+
+// export default App;
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useEffect, useState } from 'react';
 import '../scss/style.scss'
-import Kobe from '../assets/KobeBryant.jpg'
+import Kobe from '../assets/KobeBryant.jpeg'
 import Bill from '../assets/BillRussell.jpeg'
 import Kareem from '../assets/Kareem.jpeg'
 import Larry from '../assets/LarryBird.jpeg'
@@ -10,7 +10,7 @@ import Michael from '../assets/MichaelJordan.jpeg'
 import Shaq from '../assets/Shaq.jpeg'
 import Wilt from '../assets/WiltChamberlain.jpeg'
 
-const cardData = [
+const goatData = [
   { player: Kobe, id: 1 },
   { player: Michael, id: 2 },
   { player: Lebron, id: 3 },
@@ -18,56 +18,37 @@ const cardData = [
 ];
 
 const App = () => {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const handleOnMouseMove = (e) => {
-      const { currentTarget: target } = e;
-      const rect = target.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
+  return (
+    <div>
+      <GoatBox />
+    </div>
+  );
+};
 
-      setMousePosition({ x, y });
-    };
-
-    const cards = document.querySelectorAll('.card');
-
-    cards.forEach((card) => {
-      card.addEventListener('mousemove', handleOnMouseMove);
-
-      return () => {
-        card.removeEventListener('mousemove', handleOnMouseMove);
-      };
-    });
-
-    return () => {
-      cards.forEach((card) => {
-        card.removeEventListener('mousemove', handleOnMouseMove);
-      });
-    };
-  }, []); 
-
+const GoatBox = () => {
+  
+  
   return (
     <>
       <h1 id="header">Greatest Of All Time</h1>
       <div id="cards">
-        {cardData.map((card) => (
+        {goatData.map((card) => (
           <div
             key={card.id}
             className="card"
-            style={{
-              '--mouse-x': `${mousePosition.x}px`,
-              '--mouse-y': `${mousePosition.y}px`,
-              backgroundImage: `url(${card.player})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
+              
+                style={{
+                  backgroundImage: `url(${card.player})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
             }}
           ></div>
         ))}
       </div>
     </>
   );
-};
+}
 
 
 export default App;
